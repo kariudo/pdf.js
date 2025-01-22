135ab34685309bbb40da0f94dfe5ce32f4917ac2
Hunter Horsman <kariudo@gmail.com>
2025-01-22 18:20:01 +0000

    build(npm): fix url in publish chain for scope

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2270,8 +2270,8 @@ function packageJson() {
   const DIST_DESCRIPTION = "Generic build of Mozilla's PDF.js library.";
   const DIST_KEYWORDS = ["Mozilla", "pdf", "pdf.js"];
   const DIST_HOMEPAGE = "https://mozilla.github.io/pdf.js/";
-  const DIST_BUGS_URL = "https://github.com/mozilla/pdf.js/issues";
-  const DIST_GIT_URL = "https://github.com/mozilla/pdf.js.git";
+  const DIST_BUGS_URL = "https://github.com/kariudo/pdf.js/issues";
+  const DIST_GIT_URL = "https://github.com/kariudo/pdf.js";
   const DIST_LICENSE = "Apache-2.0";
 
   const npmManifest = {
